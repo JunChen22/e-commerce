f303fb961baa27cc2040371e32d1475a7568317e
JunChen22 <ccjh522@gmail.com>
2020-03-02 23:54:04 -0500

    change batis to use mapper xml to replace annotation

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 #### Spring E-Commerce 
 [![Build Status](https://travis-ci.com/JunChen22/Spring-E-commerce.svg?branch=master)](https://travis-ci.com/JunChen22/Spring-E-commerce)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Material used:
+- https://www.youtube.com/watch?v=EpYBP7EZ8Y4
+- https://www.youtube.com/watch?v=-PcU6uf-p3g&list=PLA7e3zmT6XQUNzbNoTb-8c-VTa_i_w5-y
--- src/main/java/com/example/spring/ecommerce/demo/controller/BrandController.java
+++ src/main/java/com/example/spring/ecommerce/demo/controller/BrandController.java
@@ -4,12 +4,12 @@ package com.example.spring.ecommerce.demo.controller;
 import com.example.spring.ecommerce.demo.mapper.BrandMapper;
 import com.example.spring.ecommerce.demo.model.Brand;
 import com.example.spring.ecommerce.demo.service.BrandService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/brand")
@@ -18,18 +18,17 @@ public class BrandController {
     @Autowired
     BrandService brandService;
 
-    @Autowired
-    BrandMapper brandMapper;
+    private static final Logger LOGGER = LoggerFactory.getLogger(BrandController.class);
 
-
-    @GetMapping("/create")
-    public ResponseEntity<?> create(){
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody Brand brand){
+        LOGGER.debug("createBrand success:{}",brand);
         return new ResponseEntity<>("at brand create", HttpStatus.CREATED);
     }
 
     @GetMapping("/getall")
     public Iterable<Brand> listAllBrand(){
-        return brandMapper.findAll();
+        return null;
     }
 
     /*
